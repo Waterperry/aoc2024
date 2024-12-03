923a22d76a4aceea04950ebdde7659a4bd6a4ff4
Waterperry <43050523+Waterperry@users.noreply.github.com>
2024-12-03 18:14:26 +0000

    add day2 oneliner

--- day2/day2.py
+++ day2/day2.py
@@ -1,8 +1,10 @@
 with open("input") as f:
     lines: list[str] = [line.strip() for line in f.readlines()]
 
+
 def abs(x: int) -> int:
-    return x if x >= 0 else x*-1
+    return x if x >= 0 else x * -1
+
 
 def check_safe(line: list[int]) -> bool:
     direction: str = "increasing" if line[1] > line[0] else "decreasing"
@@ -10,11 +12,12 @@ def check_safe(line: list[int]) -> bool:
         diff = abs(l - r)
         if l > r and direction == "increasing":
             return False
-        if l < r and direction == "decreasing": 
+        if l < r and direction == "decreasing":
             return False
         if not (1 <= diff <= 3):
             return False
     return True
+
 
 def check_safe_tolerant(line: list[int], is_sublist: bool = False) -> bool:
     direction: str = "increasing" if line[1] > line[0] else "decreasing"
@@ -23,7 +26,7 @@ def check_safe_tolerant(line: list[int], is_sublist: bool = False) -> bool:
         diff = abs(l - r)
         if l > r and direction == "increasing":
             is_safe = False
-        if l < r and direction == "decreasing": 
+        if l < r and direction == "decreasing":
             is_safe = False
         if not (1 <= diff <= 3):
             is_safe = False
@@ -33,34 +36,15 @@ def check_safe_tolerant(line: list[int], is_sublist: bool = False) -> bool:
     else:
         for i in range(len(line)):
             sublist = line.copy()
-            _  = sublist.pop(i)
+            _ = sublist.pop(i)
             if check_safe(sublist):
                 return True
     return False
 
+# part 1
 print(sum(int(check_safe([int(x) for x in line.split(" ")])) for line in lines))
-print(
-    sum(
-        int(
-            all((-3 <= (l - r) <= -1) or (1 <= (l - r) <= 3) for l, r in zip([int(x) for x in line.split(" ")][:-1], [int(x) for x in line.split(" ")][1:]))
-            and
-            (
-                all(
-                    l < r for l, r in zip(
-                        [int(x) for x in line.split(" ")][:-1],
-                        [int(x) for x in line.split(" ")][1:]
-                    )
-                )
-                or
-                all(
-                    r < l for l, r in zip(
-                        [int(x) for x in line.split(" ")][:-1],
-                        [int(x) for x in line.split(" ")][1:]
-                    )
-                )
-            )
-        )
-        for line in open("input").readlines()
-    )
-)
+print( sum( int( all( (-3 <= (l - r) <= -1) or (1 <= (l - r) <= 3) for l, r in zip([int(x) for x in line.split(" ")][:-1], [int(x) for x in line.split(" ")][1:])) and ( all( l < r for l, r in zip([int(x) for x in line.split(" ")][:-1], [int(x) for x in line.split(" ")][1:])) or all( r < l for l, r in zip([int(x) for x in line.split(" ")][:-1], [int(x) for x in line.split(" ")][1:])))) for line in open("input").readlines()))
+
+# part 2
 print(sum(int(check_safe_tolerant([int(x) for x in line.split(" ")])) for line in lines))
+print(sum(int(all((-3 <= (l - r) <= -1) or (1 <= (l - r) <= 3) for l, r in zip([int(x) for x in line.split(" ")][:-1], [int(x) for x in line.split(" ")][1:])) and (all(l < r for l, r in zip([int(x) for x in line.split(" ")][:-1], [int(x) for x in line.split(" ")][1:])) or all(r < l for l, r in zip([int(x) for x in line.split(" ")][:-1], [int(x) for x in line.split(" ")][1:])))) or any(all((-3 <= (l - r) <= -1) or (1 <= (l - r) <= 3) for l, r in zip(([int(x) for x in line.split(" ")][:i] + [int(x) for x in line.split(" ")][i+1:])[:-1], ([int(x) for x in line.split(" ")][:i] + [int(x) for x in line.split(" ")][i+1:])[1:])) and (all(l < r for l, r in zip(([int(x) for x in line.split(" ")][:i] + [int(x) for x in line.split(" ")][i+1:])[:-1], ([int(x) for x in line.split(" ")][:i] + [int(x) for x in line.split(" ")][i+1:])[1:])) or all(r < l for l, r in zip(([int(x) for x in line.split(" ")][:i] + [int(x) for x in line.split(" ")][i+1:])[:-1], ([int(x) for x in line.split(" ")][:i] + [int(x) for x in line.split(" ")][i+1:])[1:]))) for i in range(len(line.split(" ")))) for line in open("input").readlines()))
